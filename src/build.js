6d72a0769072dcdce497ffdf131a0f395acc84a1
uid-11222 <uid.11222@gmail.com>
2016-09-09 11:48:41 +0300

    Add creating build dir.

--- src/build.js
+++ src/build.js
@@ -2,10 +2,16 @@
 
 'use strict'; /* global process */
 
+const fs = require(`fs`);
 
-/**
- * If called from command line, execute with it args.
- */
-if (require.main && require.main.id === module.id) {
-  checkHistory(process.argv[2]);
+const BUILD = `./build/`,
+      param = process.argv[2];
+
+
+try {
+  fs.accessSync(BUILD);
+} catch (e) {
+  fs.mkdirSync(BUILD);
 }
+
+console.log(process.argv);
